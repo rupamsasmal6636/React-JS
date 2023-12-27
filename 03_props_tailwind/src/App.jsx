b3cf22ad1598a26cf6d35cd5fd5d7b4173990ba8
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2023-12-27 14:20:17 +0530

    learn props

--- 03_props_tailwind/src/App.jsx
+++ 03_props_tailwind/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import Card from './components/Card'
+import Card2 from './components/Card2'
 
 function App() {
-
+  let arr=[1,2,3];
+  let obj={ name:"Apple", price: 999};
   return (
     <>
       <h1 className="font-bold bg-green-400 text-black rounded-xl p-2">
         Tailwind CSS Test
       </h1>
+      {/* Now we can pass props/property to each component  */}
+      <Card username= "Rupam" btntxt="visit profile"/>
+      <Card username="Aniket"/>
+      <Card2 product="Macbook Air" myArr={arr} data={obj}/>
     </>
   )
 }
