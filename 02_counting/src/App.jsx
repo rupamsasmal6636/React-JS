bcba2077b815fd9341a5118e0011d746bfddac62
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2023-12-27 21:25:32 +0530

    learn async property of useState set method

--- 02_counting/src/App.jsx
+++ 02_counting/src/App.jsx
@@ -19,7 +19,27 @@ function App() {
   
   const [count, setCount] = useState(0) // here 0 is the initial value, it can be anything, count is variable to store data and setCount is function to change count
   const increase = ()=>{
+    // normal struff
     setCount(count+1)
+
+    // some interesting stuff regarding interview:
+    // what will happen if we call the method multiple times? 
+    setCount(count+1)
+    setCount(count+1)
+    setCount(count+1)
+    setCount(count+1)
+    /* The reason the count is not updated +4 times as you might expect is because the setCount function in React is asynchronous. 
+    When you call setCount, React schedules an update to the state, but the actual state update may not happen immediately. 
+    React batches state updates for performance reasons, and as a result, the count value inside your increase function 
+    might not be updated immediately after each call to setCount.
+    */
+    setCount((count)=>count+1)
+    setCount((count)=>count+1)
+    setCount((count)=>count+1)
+    /* This code will work correctly. When you use the functional form of setCount and pass a function that takes the previous state 
+    as an argument, React ensures that you are always working with the latest state. 
+    This approach helps to avoid potential issues related to the asynchronous nature of state updates in React.
+    */
   }
   const decrease = ()=>{
     setCount(count-1)
