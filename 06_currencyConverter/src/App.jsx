fbbb6345429a67df6ee261d78cd18676f01464fd
Rupam Sasmal <rupam.sasmal.6636@gmail.com>
2024-01-11 23:56:02 +0530

    create custom hooks and components

--- 06_currencyConverter/src/App.jsx
+++ 06_currencyConverter/src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
 
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      
+
+    </div>
   )
 }
 
---
+++ 06_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+// Custom hooks is nothing but a function that return something
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data,setData]=useState({})
+    useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res[currency]))
+    },[currency])
+    return data;
+}
+
+export default useCurrencyInfo;
